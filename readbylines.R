cfbe366a716ee49ec7a2f951b6a31dfd9eeb8086
James Clawson <clawson@gmail.com>
2016-06-15 14:47:19 -0700

    Corrected a couple typos

--- readbylines.R
+++ readbylines.R
@@ -1,6 +1,6 @@
 # testing with one file
 # save the path to the file
-data.dir <- "data/1/1/000006.xml"
+data.dir <- "data/1/1/000006/000006.xml"
 
 # read all the lines into a vector
 filelines <- readLines(data.dir)
@@ -18,5 +18,5 @@ filelines <- readLines(data.dir)
 # articletype
 
 authorgrep <- # figure out general expression here
-author <- grep(authorgrep, filelines, value=FALSE) # gather line contents for author
+author <- grep(authorgrep, filelines, value=TRUE) # gather line contents for author
 # author <- grep(authorgrep, filelines, value=FALSE) # gather vector addresses for author
