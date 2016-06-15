95d5e24c2b64bcdf35ae154ec6fca9db2ebbb8b1
James Clawson <clawson@gmail.com>
2016-06-15 15:28:30 -0700

    Switching to scrape through the subdirectories

--- readbylines.R
+++ readbylines.R
@@ -2,10 +2,10 @@
 # save the path to the file
 filelist <- list.files(path="data",pattern="*xml",recursive=TRUE)
 
-data.dir <- "data/1/1/000006/000006.xml"
+# data.dir <- "data/1/1/000006/000006.xml"
 
 # read all the lines into a vector
-filelines <- readLines(data.dir)
+# filelines <- readLines(data.dir)
 
 # Collect important data
 # number of authors (or just plurality?)
@@ -19,11 +19,22 @@ filelines <- readLines(data.dir)
 # keywords
 # articletype
 
-authorgrep <- "<author>([A-Za-z ]*)<\\/author>" # figure out general expression here
-author <- grep(authorgrep, filelines, value=TRUE) # gather line contents for author
-# author <- grep(authorgrep, filelines, value=FALSE) # gather vector addresses for author
+alldocs <- matrix()
+for (file in filelist) {
+  file <- paste("data/",file,sep="")
+  filelines <- readLines(file)
+  
+  fileparse <- matrix()
+  
+  authorgrep <- "^/s<author>([A-Za-z ]*)<\\/author>" # figure out general expression here
+  author <- grep(authorgrep, filelines, value=TRUE) # gather line contents for author
+  
+  titlegrep <- "^/s<title>([A-Za-z ,]*)<\\/title>" # figure out general expression here
+  title <- grep(titlegrep, filelines, value=TRUE) # gather line contents for title
+  
+  fileparse <- cbind(fileparse,author,title)
+  
+  alldocs <- rbind(alldocs,fileparse)
+}
 
-titlegrep <- "<title>([A-Za-z ,]*)<\\/title>" # figure out general expression here
-title <- grep(titlegrep, filelines, value=TRUE) # gather line contents for author
-# author <- grep(authorgrep, filelines, value=FALSE) # gather vector addresses for author
-
+print(alldocs)
