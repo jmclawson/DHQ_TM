7719a062d3af4938b370a46c73852b1239e026ab
James Clawson <clawson@gmail.com>
2016-06-15 15:59:27 -0700

    update process, still doesn't work

--- readbyXML.R
+++ readbyXML.R
@@ -2,15 +2,15 @@ library(XML)
 
 filelist <- list.files(path="data",pattern="*xml",recursive=TRUE,full.names=T)
 
-# docs <- c()
-# for (file in filelist) {
-#   doc <- xmlTreeParse(file, useInternalNodes=TRUE)
-#   docs <- c(docs,doc)
-# }
-
 docbodies <- c()
 for (file in filelist) {
   doc <- xmlTreeParse(file, useInternalNodes=TRUE)
   docbody <- getNodeSet(doc, "/tei:TEI//tei:body",namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
   docbodies <- c(docbodies,docbody)
+}
+
+for (dbody in 1:length(docbodies)) {
+  articlefile = paste("data/texts/",dbody,sep="")
+  articlefile = paste(articlefile,".txt",sep="")
+  write(docbodies[dbody],file=articlefile,append=FALSE,sep="") 
 }
