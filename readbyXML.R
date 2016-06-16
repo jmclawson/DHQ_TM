976907e7e81384f8f3b4277c48f38e714ecb8e1c
James Clawson <clawson@gmail.com>
2016-06-15 20:08:44 -0700

    It works! Run "readbyXML.R" and you should get a new folder in "data" called "texts."

--- readbyXML.R
+++ readbyXML.R
@@ -5,11 +5,10 @@ filelist <- list.files(path="data",pattern="*xml",recursive=TRUE,full.names=T)
 docbodies <- c()
 for (file in 1:length(filelist)) {
   doc <- xmlTreeParse(filelist[file], useInternalNodes=TRUE)
-  docbody <- getNodeSet(doc, "/tei:TEI//tei:body",namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
-  articlefile = paste("data/texts/",file,sep="")
-  articlefile = paste(articlefile,".txt",sep="")
-  write(docbody,file=articlefile,append=FALSE,sep="") 
+  docextract <- getNodeSet(doc, "/tei:TEI//tei:text",namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
+  doc.body <- xmlElementsByTagName(docextract[[1]],"body")
+  doc.body.v <- xmlValue(doc.body[[1]])
+  articlefile <- paste("data/texts/",file,sep="")
+  articlefile <- paste(articlefile,".txt",sep="")
+  write(doc.body.v,file=articlefile,append=FALSE,sep="")
 }
-
-# This gives us an error:
-# Error in cat(list(...), file, sep, fill, labels, append) : argument 1 (type 'list') cannot be handled by 'cat'
