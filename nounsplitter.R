d42e7a78dcbbccf03111063306fa20e7439cd5a6
James Clawson <clawson@gmail.com>
2016-07-07 19:38:53 -0500

    Added a script for stripping out everything but common nouns, as recommended by Matthew Jockers: http://www.matthewjockers.net/2013/04/12/secret-recipe-for-topic-modeling-themes/
    Note: Ted Underwood notes that it may be more accurate to imagine Topic Modeling as "discourse" modeling, suggesting that it might be better also to keep some other parts of speech---though proper nouns are seldom useful, especially in fiction: https://tedunderwood.com/2012/04/01/what-kinds-of-topics-does-topic-modeling-actually-produce/

---
+++ nounsplitter.R
@@ -0,0 +1,29 @@
+library(NLP) 
+library(openNLP)
+
+# credit due to http://stackoverflow.com/questions/30995232/how-to-use-opennlp-to-get-pos-tags-in-r
+## SET "data.dir" to a directory containing text files
+#### It's not designed for nested folders, as I feared data loss, so do one level at a time
+## SET "saveas.file" to the destination directory
+#### The resulting file has an "n" prepended to the file name. Whether you reuse the same directory is your choice.
+data.dir <- "data/txt/Gavin/"
+saveas.file <- data.dir # feel free to save into a different folder if preferred
+
+file.list <- list.files(path=data.dir) # get all the filenames in the path
+
+for (text in file.list) { # run the loop for every file in the folder
+  txt <- readLines(paste(data.dir, text, sep=""))
+  txt <- as.String(txt)
+  wordAnnotation <- annotate(txt, list(Maxent_Sent_Token_Annotator(), Maxent_Word_Token_Annotator()))
+  POSAnnotation <- annotate(txt, Maxent_POS_Tag_Annotator(), wordAnnotation)
+  POSwords <- subset(POSAnnotation, type == "word")
+  tags <- sapply(POSwords$features, '[[', "POS")
+  # The next line searches for tagged common nouns (NN); change it for other part-of-speech tags
+  thisPOSindex <- grep("NN$", tags)
+  tokenizedAndTagged <- sprintf("%s/%s", txt[POSwords][thisPOSindex], tags[thisPOSindex])
+  untokenizedAndTagged <- paste(tokenizedAndTagged, collapse = " ")
+  # "NN" in the next line signifies common nouns
+  untokenizedAndTagged <- gsub("\\/NN", "", untokenizedAndTagged)
+  save.text <- paste(saveas.file, "n", text, sep="")
+  write(untokenizedAndTagged, file=save.text, append = FALSE, sep="")
+}
